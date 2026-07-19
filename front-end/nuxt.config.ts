ffbbdb3c7c199f58dec5c6d1db675a5552b86e4f
Anastasiia Balieieva <baleevaanastasiz@gmail.com>
2026-07-19 18:05:02 +0200

    chore: define custom colors and fonts in theme

--- front-end/nuxt.config.ts
+++ front-end/nuxt.config.ts
@@ -1,14 +1,26 @@
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  devServer:{
-    port:29563
-  },
-  vite:{
-    plugins:[tailwindcss()]
-  },
-  css:['~/assets/css/main.css']
+	compatibilityDate: '2025-07-15',
+	devtools: {enabled: true},
+
+	devServer: {
+		port: 29563
+	},
+
+	vite: {
+		plugins: [tailwindcss()]
+	},
+
+	css: ['~/assets/css/main.css'],
+	modules: ['@nuxt/fonts'],
+	fonts: {
+		adobe: {
+			id: 'dhn6rwq'
+		},
+		families: [{name: 'Ariana Pro', provider: 'adobe'},
+      {name:'All Round Gothic',provider:'adobe'}
+    ]
+	}
 })
