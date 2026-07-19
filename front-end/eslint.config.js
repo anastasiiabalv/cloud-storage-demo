ec481019bb957ba0f2475311c4cab857efeb924a
Anastasiia Balieieva <baleevaanastasiz@gmail.com>
2026-07-19 12:50:47 +0200

    chore: set up prettier and eslint

---
+++ front-end/eslint.config.js
@@ -0,0 +1,18 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import {defineConfig} from 'eslint/config'
+import eslintConfigPrettier from 'eslint-config-prettier'
+
+export default defineConfig([
+  {
+    "ignores":['.nuxt/','node_modules/','.output/']
+  },
+	{
+		files: ['**/*.{js,mjs,cjs}'],
+		plugins: {js},
+		extends: ['js/recommended'],
+		languageOptions: {globals: globals.browser}
+	},
+  eslintConfigPrettier
+
+])
